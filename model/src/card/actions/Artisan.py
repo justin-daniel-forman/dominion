4f957f6a1927b0ecde7ff661e1353644e3f8ca98
Justin Forman <justin.daniel.forman@gmail.com>
2020-04-05 18:22:46 -0500

    Create stubs for model files

---
+++ model/src/card/actions/Artisan.py
@@ -0,0 +1,4 @@
+# class Artisan : ActionCard
+
+# This is just an example of where we would put specific action cards
+# that inherit ActionCard
---
+++ model/test/card/CardTest.py
@@ -0,0 +1,4 @@
+# class CardTest
+
+# I'm not sure exactly what will go in here. It might make more sense
+# to include specific card tests in a subdirectory.
